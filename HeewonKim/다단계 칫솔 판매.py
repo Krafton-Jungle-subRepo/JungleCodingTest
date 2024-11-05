3b62e33ee2187b2a37c325063c6fb38a53d1beae
김희원 <julian3306@naver.com>
2024-11-05 13:11:29 +0900

    2024/11/5(화) [김희원] 백준 12904 A와 B, 프로그래머스 다단계 칫솔 판매 수정

--- HeewonKim/다단계 칫솔 판매.py
+++ HeewonKim/다단계 칫솔 판매.py
@@ -1,7 +1,22 @@
-import sys
-inputList = list(sys.stdin.readline().split(','))
-enroll = list(sys.stdin.readline().split())
-referral = list(sys.stdin.readline().split())
-seller = list(sys.stdin.readline().split())
-amount = list(map(int, sys.stdin.readline().split()))
-ㄷ
+def solution(enroll, referral, seller, amount):
+    
+    parent = {}
+    res = {}
+    for i in range(len(referral)):
+        parent[enroll[i]] = referral[i]
+        res[enroll[i]] = 0
+
+    def findRoot(child, cost):
+        while (cost >= 1): 
+            give_to_parent = cost // 10
+            res[child] += cost - give_to_parent
+            child = parent[child]
+            cost = give_to_parent
+            if child == '-':
+                break
+
+    for i in range(len(seller)):
+        findRoot(seller[i], amount[i]*100)
+
+    answer = list(res.values())
+    return answer

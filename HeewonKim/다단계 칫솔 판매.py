52f04daabe791e2b14338eee5cab835aab70aa45
김희원 <julian3306@naver.com>
2024-11-05 13:05:38 +0900

    2024/11/5(화) [김희원] 백준 12904 A와 B, 프로그래머스 다단계 칫솔 판매

---
+++ HeewonKim/다단계 칫솔 판매.py
@@ -0,0 +1,7 @@
+import sys
+inputList = list(sys.stdin.readline().split(','))
+enroll = list(sys.stdin.readline().split())
+referral = list(sys.stdin.readline().split())
+seller = list(sys.stdin.readline().split())
+amount = list(map(int, sys.stdin.readline().split()))
+ㄷ
